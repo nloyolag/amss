e109d01e949b2d182d6d893c6d34df760087c553
Noe Loyola <noeloyolag@gmail.com>
2015-03-09 11:32:08 -0600

    Merged Polo task template

--- client/views/dashboard/task.js
+++ client/views/dashboard/task.js
@@ -7,11 +7,11 @@
 Template.task.helpers({
     
     taskEmployer: function() {
-    	return Meteor.users.findOne(this.employer).profile.name;
+    	return Meteor.users.findOne(this.task.employer).profile.name;
     },
 
     taskEmployee: function() {
-    	return Meteor.users.findOne(this.employee).profile.name;
+    	return Meteor.users.findOne(this.task.employee).profile.name;
     }
 
 });
